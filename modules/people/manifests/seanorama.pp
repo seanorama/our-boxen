76af89e6e6c33ca3ee8053a2a19c6ca1ddf93940
Sean Roberts <seanorama@gmail.com>
2013-05-26 10:45:33 +0100

    should have removed the other homebrew first. Bunch of stuff effed

--- modules/people/manifests/seanorama.pp
+++ modules/people/manifests/seanorama.pp
@@ -1,6 +1,74 @@
-class people::seanorama {
-#  include emacs   # requires emacs module in Puppetfile
-#  include sparrow # requires sparrow module in Puppetfile
+class people::seanorama 
+{
+  ## Remove the # from any of the below to install that software
+  #include alfred
+  #include caffeine
+  #include chrome
+  #include clojure
+  #include colloquy
+  #include dropbox
+  #include fitbit
+        #include firefox
+        #include flux
+  #include gitx
+  #include hub
+  #include iterm2
+  #include kindle
+  #include macvim
+  #include mongodb
+  #include mysql
+  #include nvm
+  #include onepassword
+  #include osx
+        #include python
+  #include qt
+  #include screen
+        #include secondbar
+  #include skype
+  #include sparrow
+  #include spotify
+  #include sublime_text_2
+  #include textmate
+  #include things
+        #include thunderbird
+        #include virtualbox
+  #include vlc
+  #include wget
+  #include xquartz
+  #include zsh)
+
+        #include git
+#git::config::local { '/path/to/my/repo':
+#ensure => present,
+#key    => 'user.email',
+#value  => 'turnt@example.com'
+#}
+
+        #git::config::global { 'user.email':
+            #value  => 'seanorama@gmail.com'
+        #}
+
+
+    #include vagrant
+#vagrant::plugin { 'vagrant-vmware-fusion':
+#license => 'puppet:///modules/people/joe/licenses/fusion.lic',
+#}
+
+#vagrant::box { 'squeeze64/vmware_fusion':
+#source => 'https://s3.amazonaws.com/github-ops/vagrant/squeeze64-6.0.7-vmware_fusion.box'
+#}
+
+
+  ## See modules/teams/manifests for things you can include here
+  #include teams::infrastructure
+
+  ## You can also include particular projects from modules/projects/manifests
+  #include projects::deployment
+
+  ## Here's how to include a personal github project
+  #repository { "${boxen::config::srcdir}/dotfiles":
+  #  source  => 'yourusername/dotfiles',
+  #}
 
 #  $home     = "/Users/${::luser}"
 #  $my       = "${home}/my"
@@ -10,8 +78,14 @@ class people::seanorama {
 #    source  => 'seanorama/dotfiles',
 #    require => File[$my]
 #  }
-package { [
-           'ack',
+  ## This is how to install Homebrew packages
+  ## You can get a list of your current packages with
+  ## `brew list`
+  package {
+    [
+      'autoconf',
+      'autojump',
+      'automake',
            'cloog',
            'gdbm',
            'gettext',
@@ -32,6 +106,7 @@ package { [
            'r',
            'readline',
            'rename',
+           'screen',
            'sqlite',
            'vim',
            'wget',
@@ -40,4 +115,5 @@ package { [
           ensure => 'present',
 }
 }
+
 
